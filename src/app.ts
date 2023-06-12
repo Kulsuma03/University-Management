875f2548fde5764b54c52f668b12fed7b5d767a3
Kulsuma03 <kulsumams72@gmail.com>
2023-06-13 01:10:47 +0600

    add vs code json

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import usersRouter from './app/modules/users/user.route'
 
 const app: Application = express()
 
@@ -9,9 +10,11 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.use('/api/v1/users', usersRouter)
+
 // testing
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
+  res.send('University Management Start Successfully')
 })
 
 export default app
